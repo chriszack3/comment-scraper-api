59ee7cc86b78e7836cf71e18b7d21df48d67bf82
chriszack3 <chriszack37377@gmail.com>
2022-10-03 15:39:35 -0400

    express syntax change

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ const port = process.env.PORT || 3000
 app.get('/', async (req, res) => {
   try {
     const data = await runScraper()
-    res.status(200).json({ data: data })
+    res.send({ data: data })
   }
   catch (err) {
-    res.status(500).json({ error: err })
+    res.send({ error: err })
   }
   // res.send('Hello World!')
 })
