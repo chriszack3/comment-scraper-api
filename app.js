3a394f454aca0499ee51ab31d2dc01919139ad44
chrisazack <czack@MSI.localdomain>
2022-10-03 14:58:56 -0400

    comments scraped and returned

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-const express = require('express')
+import express from 'express'
+import runScraper from './lib/getComments.js'
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+app.get('/', async (req, res) => {
+  try {
+    const data = await runScraper()
+    res.status(200).json({ data: data })
+  }
+  catch (err) {
+    res.status(500).json({ error: err })
+  }
+  // res.send('Hello World!')
 })
 
 app.listen(port, () => {
